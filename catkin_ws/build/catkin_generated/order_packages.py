c609f08ab7f435d01d617c8295b6e4135e274a94
Ricardo <ricardo.gordonsanchez@gmail.com>
2023-12-04 14:54:47 -0500

    beginning

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ricardo/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ricardo/catkin_ws/devel;/home/ricardo/ws_moveit/devel;/opt/ros/noetic'.split(';') if '/home/ricardo/catkin_ws/devel;/home/ricardo/ws_moveit/devel;/opt/ros/noetic' != '' else []
